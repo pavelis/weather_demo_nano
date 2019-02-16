cdd49040f9d5484b4b72186bb83741753219ae5e
Pavel Isayenko <pavel.isayenko@gmail.com>
2019-02-16 16:20:43 +0600

    Initial commit

---
+++ weather_demo_nano.ino
@@ -0,0 +1,137 @@
+// using Sparkfun BMP180 library https://github.com/sparkfun/BMP180_Breakout_Arduino_Library
+//#include <SFE_BMP180.h>
+// using BMP085/180 library from Filipe Vieira
+#include <BMP085.h>
+// using Adafruit DHT library https://github.com/adafruit/Adafruit_Sensor
+#include <DHT.h>
+#include <Wire.h>
+// using LiquidCrystal_I2C library from https://github.com/marcoschwartz/LiquidCrystal_I2C
+#include <LiquidCrystal_I2C.h>
+
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define AVGNUM 5 // number of measurements in average value
+#define ALT 76000 // altitude is 760 meters
+
+DHT dht (DHTPIN, DHTTYPE);
+//SFE_BMP180 bmp;
+BMP085 bmp;
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // Set the LCD I2C address
+
+byte counter;
+float hum, tempdht;
+float tempdhtavg, humavg;
+long tempbmp, pres;
+float tempbmpavg, presavg;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Starting... ");
+  delay(1000);
+  dht.begin();
+  tempdht = dht.readTemperature();
+  hum = dht.readHumidity();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(tempdht) || isnan(hum)) {
+    Serial.println("Failed to read from DHT sensor!");
+    lcd.print("DHT failure");
+    return;
+  }
+//  bmp.init(MODE_ULTRA_HIGHRES, ALT, true);
+  bmp.init();
+/*  
+  if (bmp.begin())
+    Serial.println("BMP180 init success");
+  else {
+    Serial.println("BMP180 init fail");
+    lcd.clear();
+    lcd.print("BMP180 init fail");
+    while(1);
+  }
+*/
+}            
+
+void loop() { 
+
+  delay(2000);
+  counter++;
+  tempdht = dht.readTemperature();
+  hum = dht.readHumidity();
+
+  bmp.getPressure(&pres);
+  bmp.getTemperature(&tempbmp);
+/*  
+  status = bmp.startTemperature();
+  if (status != 0) {
+    delay(status);
+    status = bmp.getTemperature(tempbmpraw);
+    if (status != 0) {
+      Serial.print("Temperature: ");
+      Serial.print(tempbmpraw, 2);
+      Serial.println(" *C");
+      status = bmp.startPressure(3);
+      if (status != 0) {
+        delay(status);
+        status = bmp.getPressure(presraw, tempbmpraw);
+        if (status != 0) {
+          Serial.print("Pressure: ");
+          Serial.print(presraw, 2);
+          Serial.println(" hPa");
+        }
+      }
+    }
+  }
+*/  
+  tempdhtavg += tempdht;
+  humavg += hum;
+  tempbmpavg += tempbmp / 10;
+  presavg += pres / 100;
+
+  if (counter == AVGNUM) {
+    tempdhtavg /= AVGNUM;
+    humavg /= AVGNUM;
+    tempbmpavg /= AVGNUM;
+    presavg /= AVGNUM;
+    Serial.print("TempDHT: ");
+    Serial.print(tempdhtavg);
+    Serial.print((char)223);
+    Serial.print("C; ");
+    Serial.print("TempBMP: ");
+    Serial.print(tempbmpavg);
+    Serial.print("; Hum: ");
+    Serial.print(humavg);
+    Serial.print("%. ");
+    Serial.print("Pressure: ");
+    Serial.print(presavg);
+    Serial.println(" hPa.");
+    lcd.clear();
+    lcd.print("Temp: ");
+    lcd.print(tempdhtavg, 1);
+    lcd.print((char)223);
+    lcd.print("C");
+    lcd.setCursor(0, 1);
+    lcd.print("Hum: ");
+    lcd.print(humavg, 1);
+    lcd.print("%");
+    lcd.setCursor(0, 2);
+    lcd.print("Pressure: ");
+    lcd.print(presavg, 1);
+    lcd.print(" hPa");
+    lcd.setCursor(0, 3);
+    lcd.print("Temp (BMP): ");
+    lcd.print(tempbmpavg, 1);
+    lcd.print((char)223);
+    lcd.print("C");
+ 
+    tempdhtavg = 0;
+    humavg = 0;
+    tempbmpavg = 0;
+    presavg = 0;
+    counter = 0;
+  }
+    
+}
