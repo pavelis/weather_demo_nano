37025e6369bcc68afa7ff75982a9e01ccf5315ee
Pavel Isayenko <pavel.isayenko@gmail.com>
2019-02-16 23:10:23 +0600

    Changed BMP085 library to Adafruit. Added show() function

--- weather_demo_nano.ino
+++ weather_demo_nano.ino
@@ -1,5 +1,5 @@
-// using BMP085/180 library from Filipe Vieira
-#include <BMP085.h>
+// using Adafruit BMP085 library https://github.com/adafruit/Adafruit-BMP085-Library
+#include <Adafruit_BMP085.h>
 // using Adafruit DHT library https://github.com/adafruit/Adafruit_Sensor
 #include <DHT.h>
 #include <Wire.h>
@@ -12,7 +12,7 @@
 #define ALT 76000 // altitude is 760 meters
 
 DHT dht (DHTPIN, DHTTYPE);
-BMP085 bmp;
+Adafruit_BMP085 bmp;
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // Set the LCD I2C address
 
 byte counter;
@@ -27,18 +27,24 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Starting... ");
+  lcd.print("Weather Demo Nano 16 Feb 2019. ");
   delay(1000);
   dht.begin();
   tempdht = dht.readTemperature();
   hum = dht.readHumidity();
-  // Check if any reads failed and exit early (to try again).
   if (isnan(tempdht) || isnan(hum)) {
-    Serial.println("Failed to read from DHT sensor!");
-    lcd.print("DHT failure");
+    Serial.println("DHT failure!");
+    lcd.print("DHT failure.");
     return;
   }
-  bmp.init();
+  if (!bmp.begin()) {
+    Serial.println("BMP failure!");
+    lcd.print("BMP failure.");
+    return;
+  }
+  pres = bmp.readPressure() / 100;
+  tempbmp = bmp.readTemperature();
+  show(tempdht, hum, pres, tempbmp);
 }            
 
 void loop() { 
@@ -48,12 +54,12 @@ void loop() {
   tempdht = dht.readTemperature();
   hum = dht.readHumidity();
 
-  bmp.getPressure(&pres);
-  bmp.getTemperature(&tempbmp);
+  pres = bmp.readPressure();
+  tempbmp = bmp.readTemperature();
 
   tempdhtavg += tempdht;
   humavg += hum;
-  tempbmpavg += tempbmp / 10;
+  tempbmpavg += tempbmp;
   presavg += pres / 100;
 
   if (counter == AVGNUM) {
@@ -61,42 +67,47 @@ void loop() {
     humavg /= AVGNUM;
     tempbmpavg /= AVGNUM;
     presavg /= AVGNUM;
-    Serial.print("TempDHT: ");
-    Serial.print(tempdhtavg);
-    Serial.print((char)223);
-    Serial.print("C; ");
-    Serial.print("TempBMP: ");
-    Serial.print(tempbmpavg);
-    Serial.print("; Hum: ");
-    Serial.print(humavg);
-    Serial.print("%. ");
-    Serial.print("Pressure: ");
-    Serial.print(presavg);
-    Serial.println(" hPa.");
-    lcd.clear();
-    lcd.print("Temp: ");
-    lcd.print(tempdhtavg, 1);
-    lcd.print((char)223);
-    lcd.print("C");
-    lcd.setCursor(0, 1);
-    lcd.print("Hum: ");
-    lcd.print(humavg, 1);
-    lcd.print("%");
-    lcd.setCursor(0, 2);
-    lcd.print("Pressure: ");
-    lcd.print(presavg, 1);
-    lcd.print(" hPa");
-    lcd.setCursor(0, 3);
-    lcd.print("Temp (BMP): ");
-    lcd.print(tempbmpavg, 1);
-    lcd.print((char)223);
-    lcd.print("C");
- 
+
+    show(tempdhtavg, humavg, presavg, tempbmpavg);
+    
     tempdhtavg = 0;
     humavg = 0;
     tempbmpavg = 0;
     presavg = 0;
     counter = 0;
   }
-    
+
+}
+
+void show(float tempdht, float hum, float pres, float tempbmp) {
+    Serial.print("TempDHT: ");
+    Serial.print(tempdht);
+    Serial.print((char)223);
+    Serial.print("C; ");
+    Serial.print("TempBMP: ");
+    Serial.print(tempbmp);
+    Serial.print("; Hum: ");
+    Serial.print(hum);
+    Serial.print("%. ");
+    Serial.print("Pressure: ");
+    Serial.print(pres);
+    Serial.println(" hPa.");
+    lcd.clear();
+    lcd.print("Temp: ");
+    lcd.print(tempdht, 1);
+    lcd.print((char)223);
+    lcd.print("C");
+    lcd.setCursor(0, 1);
+    lcd.print("Hum: ");
+    lcd.print(hum, 1);
+    lcd.print("%");
+    lcd.setCursor(0, 2);
+    lcd.print("Pressure: ");
+    lcd.print(pres, 1);
+    lcd.print(" hPa");
+    lcd.setCursor(0, 3);
+    lcd.print("Temp (BMP): ");
+    lcd.print(tempbmp, 1);
+    lcd.print((char)223);
+    lcd.print("C");
 }
