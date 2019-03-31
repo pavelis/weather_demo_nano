0876bbc46471e5b8691c983aa091d5a6c17be49f
Pavel Isayenko <pavel.isayenko@gmail.com>
2019-03-31 12:26:26 +0600

    Prepared for adding PM sensor

--- weather_demo_nano.ino
+++ weather_demo_nano.ino
@@ -9,10 +9,12 @@
 #include <Adafruit_BMP085.h>
 // Adafruit DHT library https://github.com/adafruit/Adafruit_Sensor
 #include <DHT.h>
-// standard Arduino Wire.h library
+// standard Arduino library
 #include <Wire.h>
 // LiquidCrystal_I2C library from https://github.com/marcoschwartz/LiquidCrystal_I2C
 #include <LiquidCrystal_I2C.h>
+// SDS011 library https://github.com/lewapek/sds-dust-sensors-arduino-library
+//#include "SdsDustSensor.h"
 
 #define DHTPIN 7 // pin DHT22 is connected to
 #define DHTTYPE DHT22 // type of sensor is DHT 22  (AM2302)
@@ -23,15 +25,21 @@
 #define PHOTOPIN 0 // pin light resistor is connected to
 #define BACKLIGHTPIN 3 // pin for backlight control
 
+#define RXPIN 12 // RX pin connected to sensor's TX
+#define TXPIN 11 // TX pin connected to sensor's RX
+
 DHT dht (DHTPIN, DHTTYPE);
 Adafruit_BMP085 bmp;
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // Set the LCD I2C address
+//SdsDustSensor sds(RXPIN, TXPIN);
+
 
 byte counter;
 float hum, tempdht;
 float tempdhtavg, humavg;
 float tempbmp, pres;
 float tempbmpavg, presavg;
+int pm25, pm10;
 word light;
 
 void setup() {
@@ -41,7 +49,8 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
   lcd.init();
-  delay(1000);
+//  sds.begin();
+  delay(200);
   lcd.backlight();
   lcd.setCursor(0, 0);
   dht.begin();
@@ -59,9 +68,12 @@ void setup() {
   }
   pres = bmp.readPressure();
   tempbmp = bmp.readTemperature();
-  Serial.println("Seconds;TempDHT *C;TempBMP *C;Humidity %;Pressure hPa;Light(0-1023)");
+//  sds.setActiveReportingMode(); // ensures sensor is in 'active' reporting mode
+//  sds.setCustomWorkingPeriod(1); // sensor sends data every 1 minute
+  Serial.println("Seconds;TempDHT *C;TempBMP *C;Humidity %;Pressure hPa;PM2.5;PM10;Light(0-1023)");
+  
   lcd.clear();
-  show(tempdht, hum, pres, tempbmp, light);
+  show(tempdht, hum, pres, tempbmp, light, 0, 0);
 }            
 
 void loop() { 
@@ -87,7 +99,7 @@ void loop() {
     tempbmpavg /= AVGNUM;
     presavg /= AVGNUM;
 
-    show(tempdhtavg, humavg, presavg, tempbmpavg, light);
+    show(tempdhtavg, humavg, presavg, tempbmpavg, light, pm25, pm10);
     
     tempdhtavg = 0;
     humavg = 0;
@@ -98,7 +110,7 @@ void loop() {
 
 }
 
-void show(float tempdht, float hum, float pres, float tempbmp, word light) {
+void show(float tempdht, float hum, float pres, float tempbmp, word light, int pm25, int pm10) {
     pres /= 100;
     Serial.print(millis()/1000);
     Serial.print(";");
@@ -110,30 +122,37 @@ void show(float tempdht, float hum, float pres, float tempbmp, word light) {
     Serial.print(";");
     Serial.print(pres);
     Serial.print(";");
+    Serial.print(pm25);
+    Serial.print(";");
+    Serial.print(pm10);
+    Serial.print(";");
     Serial.print(light);
     Serial.println("");
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
     lcd.print(tempdht, 1);
+    lcd.print("/");
+    lcd.print(tempbmp, 1);
     lcd.print((char)223);
-    lcd.print("C  ");
-    lcd.setCursor(14, 0);
-    lcd.print("* ");
-    lcd.print(light);
-    lcd.print(" ");
+    lcd.print("C");
     lcd.setCursor(0, 1);
     lcd.print("Hum: ");
     lcd.print(round(hum), 1);
     lcd.print("% ");
+    lcd.setCursor(14, 1);
+    lcd.print("* ");
+    lcd.print(light);
+    lcd.print(" ");
     lcd.setCursor(0, 2);
     lcd.print("Pressure: ");
     lcd.print(pres, 1);
     lcd.print(" hPa");
     lcd.setCursor(0, 3);
-    lcd.print("Temp (BMP): ");
-    lcd.print(tempbmp, 1);
-    lcd.print((char)223);
-    lcd.print("C  ");
+    lcd.print("PM 2.5:");
+    lcd.print(pm25);
+    lcd.print(" ");
+    lcd.print("10:");
+    lcd.print(pm10);
 }
 
 void backlightControl(byte pin, word light) {
